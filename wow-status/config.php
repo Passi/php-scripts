4448a41508a2d4e85ac5730c6d77ee331e2d0527
Passi <chiruclan@t-online.de>
2011-04-28 14:21:56 +0200

    integrate mysql for all servers

--- wow-status/config.php
+++ wow-status/config.php
@@ -1,10 +1,13 @@
 <?php
-// worldserver settings
-$worldip = "localhost";
-$worldport = 8085;
-$worldname = "Worldserver";
+// database settings
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = ""; // your realm db (ex.: auth)
 // loginserver settings
-$loginip = "localhost";
-$loginport = 3724;
-$loginname = "Loginserver";
+$ip = "localhost";
+$port = 3724;
+// define color
+$conline = "00FF00";
+$coffline = "CCCCCC";
 ?>
--- wow-status/index.php
+++ wow-status/index.php
@@ -1,29 +1,34 @@
 <meta http-equiv="refresh" content="30">
 <?php 
 include "config.php";
-if (! $sock = @fsockopen("tcp://".$worldip, $worldport, $num, $error, 3))
+mysql_connect($dbhost, $dbuser, $dbpass) or die(mysql_error());
+mysql_select_db($dbname) or die(mysql_error());
+$sqls = mysql_query("select name,address,port from realmlist");
+while($sql = mysql_fetch_array($sqls)) {
+if (! $sock = @fsockopen($sql["address"], $sql["port"], $num, $error, 3))
 echo '<table width=\"100%\" border=0 cellspacing=0 cellpadding=3>
   <tr>
-    <td align=\"left\" valign=\"left\"><font color="#CCCCCC"><b>'.$worldname.'</td><td align=\"right\" valign=\"right\"><img src="offlineS.png"></td>
+    <td align=\"left\" valign=\"left\"><font color="#'.$coffline.'"><b>'.$sql["name"].'</td><td align=\"right\" valign=\"right\"><img src="offlineS.png"></td>
 
   </td></tr>'; 
 else{ 
 echo '<table width=\"100%\" border=0 cellspacing=0 cellpadding=3>
   <tr>
-    <td align=\"left\" valign=\"left\"><font color="#00FF00"><b>'.$worldname.'</td><td align=\"right\" valign=\"right\"><img src="onlineS.png"></td>
+    <td align=\"left\" valign=\"left\"><font color="#'.$conline.'"><b>'.$sql["name"].'</td><td align=\"right\" valign=\"right\"><img src="onlineS.png"></td>
   </td></tr>'; 
 fclose($sock);
 } 
-if (! $sock = @fsockopen("tcp://".$loginip, $loginport, $num, $error, 3)) 
+}
+if (! $sock = @fsockopen($ip, $port, $num, $error, 3)) 
 echo '<table width=\"100%\" border=0 cellspacing=0 cellpadding=3>
   <tr>
     <td align=\"left\" valign=\"middle\"></td>
-  <td align=\"left\" valign=\"left\"><font color="#CCCCCC"><b>'.$loginname.'</td><td align=\"right\" valign=\"right\"><img src="offlineS.png"></td>
+  <td align=\"left\" valign=\"left\"><font color="#'.$coffline.'"><b>Login</td><td align=\"right\" valign=\"right\"><img src="offlineS.png"></td>
 '; 
 else{ 
 echo '<table width=\"100%\" border=0 cellspacing=0 cellpadding=3>
   <tr>
-    <td align=\"left\" valign=\"left\"><font color="#00FF00"><b>'.$loginname.'</td><td align=\"right\" valign=\"right\"><img src="onlineS.png"></td>
+    <td align=\"left\" valign=\"left\"><font color="#'.$conline.'"><b>Login</td><td align=\"right\" valign=\"right\"><img src="onlineS.png"></td>
 
   </tr></table>'; 
 fclose($sock);
